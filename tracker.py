17e8e965d0ab3f112dbe8dfad2e7e2ef519a1104
Alon Fischer <alfischer@att.net>
2019-11-25 14:23:36 -0500

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from watchdog.observers import Observer
+from watchdog.events import *
+import time
+import os 
+
+CONFIG_FILE = "./config.json"
+TASK = "python main.py"
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        FileSystemEventHandler.__init__(self)
+
+    def on_modified(self, event):
+        if event.src_path == CONFIG_FILE:
+            print("[+] Updating configs")
+            os.system(TASK)
+
+if __name__ == "__main__":
+    observer = Observer()
+    event_handler = FileEventHandler()
+    observer.schedule(event_handler,".",True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
